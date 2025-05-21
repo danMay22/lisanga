4d35e272846af0c5b458086f9f0add2037bcba85
danMay22 <mayunga.dan22@gmail.com>
2025-05-21 18:56:52 +0200

    better work

--- src/components/home/UserMenu.tsx
+++ src/components/home/UserMenu.tsx
@@ -7,8 +7,8 @@ import {
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from "./ui/dropdown-menu";
-import { Button } from "./ui/button";
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button"
 import { CircleUser, CreditCard, LogOut, Plus, UserIcon } from "lucide-react";
 import Link from "next/link";
 
