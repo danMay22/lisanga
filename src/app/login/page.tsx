b7ca50a880d66d931788ef83ee1fd0ef915720cf
danMay22 <mayunga.dan22@gmail.com>
2025-05-24 14:12:31 +0200

    helo

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -7,7 +7,7 @@ const LoginPage = () => {
     <div className="flex items-center justify-center h-minus-135 ">
     <LoginForm />
     <div className="mx-4 lg:block rounded-lg">
-      Image src={"/pexels-rdne-7683709.jpg"} alt="hero" width={500} height={500} className="object-cover object-center w-full h-full rounded-lg shadow-lg dark:shadow-gray-700" />
+      <Image src={"/pexels-rdne-7683709.jpg"} alt="hero" width={500} height={500} className="object-cover object-center w-full h-full rounded-lg shadow-lg dark:shadow-gray-700" />
       </div>
   </div>
   
