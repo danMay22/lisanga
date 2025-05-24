ab7ea2e99a1d93d84ab5174ba878402d69e6f42c
danMay22 <mayunga.dan22@gmail.com>
2025-05-24 14:10:23 +0200

    hewloo

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,16 +4,13 @@ import Image from 'next/image';
 
 const LoginPage = () => {
   return (
-    <div className="h-screen flex items-center justify-center">
-      <div className="w-full h-full grid lg:grid-cols-2 p-4">
-        <div className="max-w-xs m-auto w-full flex flex-col items-center">
-        <LoginForm />
-        </div>
-        <div className="lg:block rounded-lg">
+    <div className="flex items-center justify-center h-minus-135 ">
+    <LoginForm />
+    <div className="mx-4 lg:block rounded-lg">
         <Image src={"/pexels-rdne-7683709.jpg"} alt="hero" width={500} height={500} className="object-cover object-center w-full h-full rounded-lg shadow-lg dark:shadow-gray-700" />
-        </div>
       </div>
-    </div>
+  </div>
+  
   );
 };
 
